e1eb40518207447fe515e481ce5ae06e6861ddc0
Johan511 <harihara.sn@gmail.com>
2023-12-08 08:18:09 +0530

    added correct python solution

---
+++ tasks/minitask1/minitask1.py
@@ -0,0 +1,11 @@
+try:
+    while True:
+        user_input = input("")
+        inputStr = ""
+        for i in user_input:
+            if i == '/':
+                inputStr += i
+            inputStr += i
+        print(eval(inputStr))
+except EOFError:
+    pass
