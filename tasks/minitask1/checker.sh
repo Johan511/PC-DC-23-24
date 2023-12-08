718c5591604130558e76386626b7d3fbfc318d03
Johan511 <harihara.sn@gmail.com>
2023-12-08 08:38:13 +0530

    updated checker script for windows compatability

--- tasks/minitask1/checker.sh
+++ tasks/minitask1/checker.sh
@@ -1,3 +1,13 @@
+if [ "$(uname)" == "Darwin" ]; then
+    PYTHON_ROOT=python3     
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    PYTHON_ROOT=python3
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    PYTHON_ROOT=python
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    PYTHON_ROOT=python
+fi
+
 g++ -std=c++17 minitask1-tcgen.cpp -o _tcgen
 g++ -std=c++17 minitask1.cpp -o _soln
 
@@ -5,7 +15,7 @@ for n in {1..100};
 do
     ./_tcgen > _tc
     ./_soln < _tc > _outputted
-    python3 minitask1.py < _tc > _expected
+    $PYTHON_ROOT minitask1.py < _tc > _expected
     cmp --silent _outputted _expected || (echo "files are different" && exit)
     echo "Test $n Passed"
 done
